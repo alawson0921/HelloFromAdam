6141c4fc6f7f1f3f9608947689e0e87e07dbdc00
FishTheFiddler <wesspam11@yahoo.com>
2022-02-05 17:17:47 -0800

    Update Main.java

--- src/lawson/Main.java
+++ src/lawson/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Adam");
+	    
+	// edit from Wes Peterson
+	System.out.println("Hey Adam, my name is Wes Peterson");
     }
 }
