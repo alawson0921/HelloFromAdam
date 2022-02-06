777f9ff77f25ec9508457cafc7d78348db37347b
alawson0921 <lawsonadam.96@gmail.com>
2022-02-05 18:53:51 -0700

    commit main.java

--- src/lawson/Main.java
+++ src/lawson/Main.java
@@ -7,6 +7,6 @@ public class Main {
         System.out.println("Hello, my name is Adam");
 	    
 	// edit from Wes Peterson
-	System.out.println("Hey Adam, my name is Wes Peterson");
+	    System.out.println("Hey Adam, my name is Wes Peterson");
     }
 }
